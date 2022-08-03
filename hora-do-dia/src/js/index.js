368e6d3ab0cdd28e5a0ff53ffe7af2b5d5cba04c
Victor Hugo <108891666+Victor-Hugo-inacio@users.noreply.github.com>
2022-08-03 15:47:51 -0300

    Utilizei bastante o conceito do método DOM e tambem as funções juntamente com as condições

--- hora-do-dia/src/js/index.js
+++ hora-do-dia/src/js/index.js
@@ -1,1 +1,29 @@
-
+window.onload = function () {
+  //elementos pegos do HTML
+  let fundo = document.getElementById('fundo');
+  let cumprimento = document.getElementById('cumprimento');
+  let horario = document.getElementById('hora');
+  let img = document.getElementById('imagem');
+  //variáveis com a hora e minutos da máquina
+  let data = new Date();
+  let hora = data.getHours()
+  let minuto = data.getMinutes()
+  //1 condição pra manha
+  if (hora >= 0 && hora < 12) {
+    cumprimento.innerText = 'BOM DIA!'
+    horario.innerText = `Agora são exatamente ${hora}:${minuto}`
+    img.src = 'src/imagens/manha.png'
+    fundo.style.backgroundImage = `url(${'src/imagens/fundo-manha.jpg'})`;
+  } else if (hora >= 12 && hora < 18){ //segunda condição pra tarde
+    cumprimento.innerText = 'BOA TARDE!'
+    horario.innerText = `Agora são exatamente ${hora}:${minuto}`
+    img.src = 'src/imagens/tarde.png'
+    fundo.style.backgroundImage = `url(${'src/imagens/fundo-tarde.jpg'})`;
+  } else { // terceira condição pra noite
+    cumprimento.innerText = 'BOA NOITE!'
+    horario.innerText = `Agora são exatamente ${hora}:${minuto}`
+    img.src = 'src/imagens/noite.png'
+    fundo.style.backgroundImage = `url(${'src/imagens/fundo-noite.jpg'})`;
+  }
+  
+};
